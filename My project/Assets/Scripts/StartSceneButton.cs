e29d52f550b6754aae5d3e75e9ed2181904d46f7
mugglewoojin <mugglewoojin@gmail.com>
2026-05-22 13:24:02 +0900

    시작 씬에 버튼 몇 개

---
+++ My project/Assets/Scripts/StartSceneButton.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartSceneButton : MonoBehaviour
+{
+    public GameObject normalPanel;
+    public GameObject settingPanel;
+
+    void Start(){
+        normalPanel.SetActive(true);
+        settingPanel.SetActive(false);
+    }
+    public void OnClickStartButton(){
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void OnClickSettingButton(){
+        if(!settingPanel.activeSelf){
+            settingPanel.SetActive(true);
+            normalPanel.SetActive(false);
+        }
+
+    }
+}
